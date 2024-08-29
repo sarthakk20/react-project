bebf3cc2e74a55aa3a6677df2f5961acd88e55e5
sarthakk <sarthak20.sonawane@gmail.com>
2024-08-29 21:57:15 +0530

    first commit

---
+++ 06themeSwitcher/src/Contexts/Theme.js
@@ -0,0 +1,14 @@
+import { useContext , createContext } from "react";
+
+export const ThemeContext = createContext({
+
+    themeMode: "light",
+    darkmode: () => {},
+    lightmode: () => {},
+})
+
+export const ThemeProvider = ThemeContext.Provider
+
+export default function useTheme(){
+    return useContext(ThemeContext);
+}
