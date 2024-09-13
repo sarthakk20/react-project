2f8d739f623c590af9503af119e93f592deb60fb
sarthakk <sarthak20.sonawane@gmail.com>
2024-09-13 18:26:59 +0530

    dump the code

--- 07todosContextLocal/src/App.jsx
+++ 07todosContextLocal/src/App.jsx
@@ -1,13 +1,64 @@
+import { useEffect } from 'react';
 import './App.css'
+import { TodoProvider } from './Contexts/TodoContext'
 import './index.css'
+import TodoForm from './Components/TodoForm';
+import TodoItem from './Components/TodoItem';
 
 function App() {
 
+  const [todos, setTodos] = useState([{}]);
+
+  const addTodo =(todo)=>{
+    setTodos((prev)=>{[{id: Date.now(), ...todo},...prev]})
+  }
+
+  const updateTodo = (id, todo) =>{
+    setTodos((prev)=> prev.map((prevId)=> prevId.id === id ? todo : prevId))
+  }
+
+  const deleteTodo = (id) =>{
+    setTodos((prev)=> prev.filter((todo)=>{todo.id !== id}))
+  }
+
+  const toggleTodo = (id) =>{
+    setTodos((prev)=> prev.map((prevId)=> prevId.id === id ? {...prevId, completed: !prevId.completed} : prevId))
+
+  }
+
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+    if(todos && todos.lenght > 0) setTodos(todos);
+
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
+
+
   return (
-    
-    <h1 className='text-3xl '
-    >To Do List</h1>
-    
+    <TodoProvider value={{todos, addTodo, updateTodo, toggleTodo, deleteTodo }}>
+    <div className="bg-[#172842] min-h-screen py-8">
+    <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+        <div className="mb-4">
+            {/* Todo form goes here */} 
+            <TodoForm/>
+        </div>
+        <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo)=>(
+              <div key={todo.id}
+              className='w-full'
+              >
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+        </div>
+    </div>
+    </div>
+    </TodoProvider>
   )
 }
 
