fa7bf852146f63ef529049b5ffe4f1068190cd35
sarthakk <sarthak20.sonawane@gmail.com>
2024-10-01 14:18:17 +0530

    starting with the mega blog project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APP_APPWRITE_URL);
+  return (
+    <>
+    <h1>A Blog using appwrite</h1>
+    </>
+  )
+}
+
+export default App
